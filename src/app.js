9da43a791402bce9d43fd251633720af1baddea6
francesadim <francesadim@gmail.com>
2022-04-18 18:06:55 -0500

    Moved forecast HTML into JS. Incoorporated the openweather API

--- src/app.js
+++ src/app.js
@@ -49,7 +49,30 @@ function displayTemperature(response) {
   dateElement.innerHTML = getDate(response.data.dt * 1000);
 }
 
-//let apiKey = "d5453a99d7bbcbcb83f0b73e111b264c";
+function getForecast(coordinates) {
+  let apiKey = "d5453a99d7bbcbcb83f0b73e111b264c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+}
+axios.get(apiUrl).then(displayForecast);
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#weather-forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecastHTML =
+    forecastHTML +
+    `
+  <div class="col-2">
+   <div class="weather-forecast-date">${day}</div>
+    <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" width="50px"> <br>
+     <span id="high-temp"> 41° </span> | <span id="low-temp"> 28° </span> 
+  </div>
+</div>
+`;
+}
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML;
+
 //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=${apiKey}&units=imperial`;
 
 function search(city) {
@@ -77,6 +100,8 @@ function displayFarenheitTemperature(event) {
   currentTempElement.innerHTML = Math.round(farenheitTemperature);
 }
 //let farenheitTemperature = null;
+
+getForecast(response.data.coord);
 
 let formElement = document.querySelector("#search-form");
 formElement.addEventListener("submit", searchForm);
